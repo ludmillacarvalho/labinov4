4dafa136a946b65366df9718b68b3289d12effd7
ludmillacarvalho <113534352+ludmillacarvalho@users.noreply.github.com>
2024-04-24 11:13:18 -0300

    Add files via upload

---
+++ sql feminicidio (1).sql
@@ -0,0 +1,30 @@
+DROP TABLE IF EXISTS dimtempo;
+CREATE TABLE dimtempo (
+        idDimtempo         INTEGER PRIMARY KEY,
+        ano                 INTEGER NOT NULL
+      
+) Engine=InnoDB;
+*/
+DROP PROCEDURE IF EXISTS carga_dimensao_tempo;
+DELIMITER //
+CREATE PROCEDURE carga_dimensao_tempo(IN startdate DATE,IN stopdate DATE)
+BEGIN
+    DECLARE currentdate DATE;
+    SET currentdate = startdate;
+    WHILE currentdate <= stopdate DO
+        INSERT INTO dimtempo VALUES (
+            YEAR(currentdate),
+            YEAR(currentdate)
+           
+            );
+        SET currentdate = ADDDATE(currentdate,INTERVAL 1 YEAR);
+    END WHILE;
+END
+//
+DELIMITER ;
+
+-- delete from  dimtempo;
+CALL carga_dimensao_tempo('2016-01-01','2021-12-31');
+
+Select * from dimtempo limit 1000;
+select * from feminicidio;
